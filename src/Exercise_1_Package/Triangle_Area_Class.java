61e2a5b18d6e37cc1378b7c4800f789a0eebb226
Ivan-0101 <jabra300@pm.me>
2025-07-29 00:04:19 +0300

    Adding Triangle_Area_Class

---
+++ src/Exercise_1_Package/Triangle_Area_Class.java
@@ -0,0 +1,13 @@
+package Exercise_1_Package;
+
+public class Triangle_Area_Class {
+    public static void main(String[] args) {
+
+        int triangleSideOne = 6;
+        int triangleHeightOne = 3;
+
+        int triangleArea = (triangleSideOne * triangleHeightOne) / 2;
+
+        System.out.println("The area of the triangle is " + triangleArea);
+    }
+}
