8165a051ceb9baededd66f7965464d5a52693a49
Ivan-0101 <jabra300@pm.me>
2025-07-28 23:24:07 +0300

    Cleanup and commit of Three_Names_Class

---
+++ src/Exercise_1_Package/Three_Names_Class.java
@@ -0,0 +1,11 @@
+package Exercise_1_Package;
+
+public class Three_Names_Class {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String secondName = "Doe";
+        String thirdName = "IV";
+
+        System.out.println(firstName + " " + secondName + " " + thirdName);
+    }
+}
