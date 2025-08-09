ffa9bef84207cddffc29aceae9ffd02908a8de62
Ivan-0101 <jabra300@pm.me>
2025-08-09 20:22:47 +0300

    Adding Numbers_Divisible_By_5 class

---
+++ src/Exercise_3_Package/Numbers_Divisible_By_5.java
@@ -0,0 +1,18 @@
+package Exercise_3_Package;
+
+public class Numbers_Divisible_By_5 {
+    public static void main (String[] args) {
+
+        int[] list1 = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+
+        for (int number : list1) {
+            if (number > 150) {
+                continue;
+            }
+
+            if (number % 5 == 0) {
+                System.out.println("Number " + number + " is divisible by 5");
+            }
+        }
+    }
+}
